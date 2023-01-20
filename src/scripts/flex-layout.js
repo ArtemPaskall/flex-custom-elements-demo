ca00b6969009745840fee20a3d6abf3f724672ef
ArtemPaskall <artempaskal@gmail.com>
2023-01-20 18:49:45 +0200

    demo version flex custom elements

---
+++ src/scripts/flex-layout.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class FlexLayout extends HTMLElement {
+
+  constructor(customElements) {
+    super()
+    
+    const shadow = this.attachShadow({ mode: 'open' })
+
+    this.style.display = 'flex'
+    this.style.flexDirection = 'row'
+    this.style.justifyContent = 'center'
+    this.children[0].setAttribute('class', '.side-indents')
+    this.children[1].setAttribute('class', '.main-content')
+
+    this.children[2].setAttribute('class', '.side-indents');
+
+    [...this.children].forEach(el => {
+      shadow.appendChild(el)
+    })
+  }
+}
+
+customElements.define('flex-layout', FlexLayout)
